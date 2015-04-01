12f0f377681198cc4124fbf63b2808c75ffd01d7
Peng Zhong <peng@nylira.com>
2015-04-01 18:53:39 +0800

    add a line example, update libs

--- pub/js/app.js
+++ pub/js/app.js
@@ -5,19 +5,21 @@
 // Modules
 
 var PIXI = require('pixi.js')
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST
+
 var maximize = require('nylira-maximize')
 
 //==============================================================================
 // Variables
 
-var GAME = {
+var Game = {
   w: 1920
 , h: 1080
 , id: document.getElementById('gameCanvas')
 }
-var SCENES = {}
+var Scenes = {}
 
-var TEXTURES = {
+var Textures = {
   grass: new PIXI.Texture.fromImage('./img/texture_grass_02.png')
 }
 
@@ -25,28 +27,40 @@ var TEXTURES = {
 // Setup
 
 function renderGrass(scene) {
-  var grass = new PIXI.extras.TilingSprite(TEXTURES.grass)
-  grass.width = GAME.w
-  grass.height = GAME.h
+  var grass = new PIXI.extras.TilingSprite(Textures.grass)
+  grass.width = Game.w
+  grass.height = Game.h
   scene.addChild(grass)
 }
 
-function renderTest(scene) {
+function renderSquare(scene) {
   var bar = new PIXI.Graphics()
   bar.beginFill(0x88CC88)
   bar.lineStyle(2, 0x000000)
-  bar.drawRect(200, 200, 200, 200)
+  bar.drawRect(50, 50, 200, 200)
   bar.endFill()
   scene.addChild(bar)
 }
 
-function setup() {
-  GAME.renderer = PIXI.autoDetectRenderer(GAME.w, GAME.h, {view: GAME.id})
-  SCENES.primary = new PIXI.Container({width: GAME.w, height: GAME.h})
-  maximize(GAME.id, GAME.w, GAME.h)
+function renderLine(scene) {
+  var line = new PIXI.Graphics()
+  line.beginFill()
+  line.lineStyle(8,0x000000)
+  line.moveTo(300, 50)
+  line.lineTo(600, 400)
+  scene.addChild(line)
+}
 
-  renderGrass(SCENES.primary)
-  renderTest(SCENES.primary)
+function setup() {
+  Game.renderer = PIXI.autoDetectRenderer(Game.w, Game.h, {view: Game.id})
+  Scenes.primary = new PIXI.Container({width: Game.w, height: Game.h})
+  Scenes.primary.scale.x = 2
+  Scenes.primary.scale.y = 2
+  maximize(Game.id, Game.w, Game.h)
+
+  renderGrass(Scenes.primary)
+  renderSquare(Scenes.primary)
+  renderLine(Scenes.primary)
 }
 
 //==============================================================================
@@ -69,7 +83,7 @@ function gameLoop() {
   // do stuff every frame
   update(dt)
 
-  GAME.renderer.render(SCENES.primary)
+  Game.renderer.render(Scenes.primary)
 
   requestAnimationFrame(gameLoop)
 }
